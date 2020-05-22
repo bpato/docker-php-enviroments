77bcd6eeb5d7c95bac6611898672dbea7e4c95d7
bpato <brais.pato@redegal.com>
2020-05-22 15:47:47 +0200

    Fix directory value

--- docker/nginx/ssl/generate-selfsigned-certificate.sh
+++ docker/nginx/ssl/generate-selfsigned-certificate.sh
@@ -1,5 +1,3 @@
 #!/usr/bin/env bash
 
-openssl req -x509 -nodes -days 365 -newkey rsa:2048 \
-    -keyout ./ssl/nginx-selfsigned.key \
-    -out ./ssl/nginx-selfsigned.crt
+openssl req -x509 -nodes -days 365 -newkey rsa:2048 -keyout ./nginx-selfsigned.key -out ./nginx-selfsigned.crt
